fa45f077cf32f5a3b2e6c0d47f7f7f83ccc98db4
V <sss.5151@mail.ru>
2021-09-17 13:11:01 +0400

    Мини слайдреы на странице, рефакторинг

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,42 @@
-import Slider from "./modules/slider";
-import VideoPlayer from './modules/videoPlayer'
+import VideoPlayer from './modules/videoPlayer';
+import MainSlider from './modules/slider/mainSlider';
+import MiniSlider from './modules/slider/sliderMini';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict'
 
-  const slider = new Slider('.page', '.next');
+  const slider = new MainSlider({
+    btns: '.next', 
+    container: '.page'
+  });
   slider.render();
+
+  const showUpSlider = new MiniSlider({
+    container: '.showup__content-slider',
+    next: '.showup__next',
+    prev: '.showup__prev',
+    activeClass: 'card-active',
+    animate: true,
+  })
+  showUpSlider.init()
+
+  const modulesSlider = new MiniSlider({
+    container: '.modules__content-slider',
+    next: '.modules__info-btns .slick-next',
+    prev: '.modules__info-btns .slick-prev',
+    activeClass: 'card-active',
+    animate: true,
+    autoplay: true,
+  })
+  modulesSlider.init()
+
+  const feedSlider = new MiniSlider({
+    container: '.feed__slider',
+    next: '.feed__slider .slick-next',
+    prev: '.feed__slider .slick-prev',
+    activeClass: 'feed__item-active',
+  })
+  feedSlider.init()
 
   const videoPlayer = new VideoPlayer('.showup .play', '.overlay')
   videoPlayer.init()
