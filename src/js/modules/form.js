41f8b048e4cbe321033e27f561a0259635342e3e
V <sss.5151@mail.ru>
2021-09-18 11:25:28 +0400

    Отправка формы__2, добавил маску номера США запретил ввод кирилицы в поле email, так же добавил проверку если вдруг в этом поле ввели кирилицу(выдаст ошибку в поле ввода)

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -1,6 +1,7 @@
 export default class Form {
   constructor(form) {
     this.forms = document.querySelectorAll(form);
+    this.inputs = document.querySelectorAll('input')
     this.message = {
       loading: 'Загрузка...',
       success: 'Спасибо! Скоро мы с вами свяжемся',
@@ -8,6 +9,87 @@ export default class Form {
     };
     this.path = 'assets/question.php';
   }
+
+  clearInputs() {
+    this.inputs.forEach(item => {
+      item.value = '';
+    })
+  }
+
+  checkMailInputs()  {
+    const txtInputs = document.querySelectorAll('[type="email"]');
+    txtInputs.forEach(input => {
+      input.addEventListener('keypress', function (e) {  
+        if (e.key.match(/[^a-z 0-9 @ /.]/ig)) {
+          e.preventDefault()
+        }
+      });
+  
+      input.addEventListener('blur', function (e) {  
+        if(input.value.match(/[а-яё]/ig)){
+          e.preventDefault()
+          input.style.color = 'red'
+          input.value = 'Введите корректное значение'
+   
+        }
+      });
+      input.addEventListener('focus', function () {  
+          input.style.color = ''
+          input.value = ''  
+      });
+      
+    });
+  }
+
+  initMask() {
+    let setCursorPosition = (pos, elem) => {
+      elem.focus();
+  
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos)
+      } else if(elem.createTextRange) {
+        let range = elem.createTextRange();
+  
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+      }
+    }
+  
+    function mask(event) {
+      let matrix = '+1 (___) ___-____',
+          i      = 0,
+          def    = matrix.replace(/\D/g, ''),
+          val    = this.value.replace(/\D/g, '');
+  
+      if (def.length >= val.length) {
+        val = def;
+      }
+  
+      this.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+      })
+  
+      if (event.type === 'blur') {
+        if (this.value.length == 2) {
+          this.value = '';
+        }
+      } else {
+        setCursorPosition(this.value.length, this);
+      }
+    }
+  
+    let inputs = document.querySelectorAll('[name="phone"]');
+  
+    inputs.forEach(input => {
+      input.addEventListener('input', mask);
+      input.addEventListener('focus', mask);
+      input.addEventListener('blur', mask);
+    })
+  }
+
+
 
   async postData(url, data) {
     let res = await fetch(url, {
@@ -19,6 +101,8 @@ export default class Form {
   }
 
   init() {
+    this.checkMailInputs();
+    this.initMask()
     this.forms.forEach(form => {
       form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -41,6 +125,12 @@ export default class Form {
             })
             .catch(() => {
               statusMessage.textContent = this.message.failure;
+            })
+            .finally(() => {
+              this.clearInputs()
+              setTimeout(() => {
+                statusMessage.remove()
+              }, 3000)
             })
       })
     })
